44e2e3dfba047656b845e7275fa54fccb1722ad1
Namokar100 <namokarsvg@gmail.com>
2025-06-26 11:52:18 +0530

    basic setup of frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import './App.css';
+import WeatherFetcher from './components/WeatherFetcher';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherFetcher />
+    </div>
+  );
+}
+
+export default App;
